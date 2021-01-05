76f1ebf38768e748ff8502c528cb3dc216bea07f
wellme <wellmey@gmail.com>
2021-01-04 22:26:28 -0500

    Initial commit

---
+++ MintTea/Class1.cs
@@ -0,0 +1,11 @@
+﻿using BepInEx;
+
+namespace MyUserName {
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.wellme.MintTea", "Mint tea", "0.0.0")]
+    public class MintTea : BaseUnityPlugin {
+        public void Awake() {
+            Logger.LogMessage("Loaded MyModName!");
+        }
+    }
+}
