94dbb136784fe129d53f4b689d23d39337cff9b3
wellme <wellmey@gmail.com>
2021-01-15 12:03:30 -0500

    Added release script

---
+++ dist/release.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import zipfile
+import json
+
+bin = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "MintTea", "bin", "Debug", "MintTea.dll"))
+manifest = os.path.abspath(os.path.join(os.path.dirname(__file__), "manifest.json"))
+icon = os.path.abspath(os.path.join(os.path.dirname(__file__), "icon.png"))
+readme = os.path.abspath(os.path.join(os.path.dirname(__file__), "readme.md"))
+
+source = os.path.join(os.path.dirname(__file__), os.pardir, "MintTea/MintTea.cs")
+
+
+def clean():
+    path = os.path.dirname(os.path.abspath(__file__))
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)) and file[-4:] == ".zip":
+            os.remove(file)
+
+def getOutFile(version):
+    return os.path.join(os.path.dirname(__file__), "MintTea-" + version + ".zip")
+
+def assertBin(version):
+    with open(bin, "rb") as file:
+        return version in str(file.read())
+    return False
+
+def assertSource(version):
+    with open(source, "rb") as file:
+        return version in str(file.read())
+    return True
+
+def assertManifest(version):
+    with open(manifest, "r") as file:
+        return json.loads(file.read())["version_number"] == version
+    return False
+
+def assertReadme(version):
+    with open(readme, "r") as file:
+        return version in str(file.read())
+    return False
+
+
+def getVersion():
+    assert len(sys.argv) == 2
+    return sys.argv[1]
+
+def pack(version):
+    with zipfile.ZipFile(getOutFile(version), "w", zipfile.ZIP_DEFLATED) as handle:
+        handle.write(bin, os.path.split(bin)[1])
+        handle.write(manifest, os.path.split(manifest)[1])
+        handle.write(icon, os.path.split(icon)[1])
+        handle.write(readme, os.path.split(readme)[1])
+
+def main():
+
+    clean()
+
+    version = getVersion()
+
+    shouldRelease = True
+    b = assertBin(version) 
+    if not b:
+        print("Wrong binary")
+    shouldRelease = shouldRelease and b
+    b = assertSource(version)
+    if not b:
+        print("Wrong source")
+    shouldRelease = shouldRelease and b
+    b = assertManifest(version)
+    if not b:
+        print("Wrong manifest")
+    shouldRelease = shouldRelease and b
+    if not assertReadme(version):
+        print("No changelog in readme!!")
+
+    if shouldRelease:
+        pack(version)
+
+if __name__ == "__main__":
+    main()
